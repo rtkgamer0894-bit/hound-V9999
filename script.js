e7fc7f71dbe6e741b6f2ffd9851bb9f16726def5
rtkgamer0894-bit <rtkgamer0894@gmail.com>
2026-03-31 09:25:40 +0700

    script.js

--- script.js
+++ script.js
@@ -1,69 +1,32 @@
-const msgerForm = get(".msger-inputarea");
-const msgerInput = get(".msger-input");
-const msgerChat = get(".msger-chat");
+function showPage(pageId){
 
-const BOT_MSGS = [
-"Hi, how are you?",
-"Turn your location on for find people near you",
-"I am powered by ChatGPT 4",
-"I feel sleepy! :("];
+document.getElementById('redeem-page').style.display="none"
+document.getElementById('detail-page').style.display="none"
 
+document.getElementById(pageId).style.display="block"
 
-// Icons made by Freepik from www.flaticon.com
-const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
-const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
-const BOT_NAME = "Admin";
-const PERSON_NAME = "User";
+window.scrollTo(0,0)
 
-msgerForm.addEventListener("submit", event => {
-  event.preventDefault();
-
-  const msgText = msgerInput.value;
-  if (!msgText) return;
-  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
-  msgerInput.value = "";
-  botResponse();
-});
-
-function appendMessage(name, img, side, text) {
-  //   Simple solution for small apps
-  const msgHTML = `
-    <div class="msg ${side}-msg">
-      <div class="msg-img" style="background-image: url(${img})"></div>
-
-      <div class="msg-bubble">
-        <div class="msg-info">
-          <div class="msg-info-name">${name}</div>
-          <div class="msg-info-time">${formatDate(new Date())}</div>
-        </div>
-
-        <div class="msg-text">${text}</div>
-      </div>
-    </div>
-  `;
-
-  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
-  msgerChat.scrollTop += 500;
 }
-function botResponse() {
-  const r = random(0, BOT_MSGS.length - 1);
-  const msgText = BOT_MSGS[r];
-  const delay = msgText.split(" ").length * 100;
 
-  setTimeout(() => {
-    appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
-  }, delay);
-}
-// Utils
-function get(selector, root = document) {
-  return root.querySelector(selector);
-}
-function formatDate(date) {
-  const h = "0" + date.getHours();
-  const m = "0" + date.getMinutes();
+function startRedeem(){
 
-  return `${h.slice(-2)}:${m.slice(-2)}`;
+const phone=document.getElementById('phone').value
+
+if(phone.length<10){
+
+alert("กรุณากรอกเบอร์โทรศัพท์ให้ถูกต้อง")
+
+return
+
 }
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+
+document.getElementById('loading-screen').style.display="flex"
+
+}
+
+function goToApp(){
+
+window.location.href="https://www.truemoney.com/"
+
 }
